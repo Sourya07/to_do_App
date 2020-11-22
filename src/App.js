c5dd7fd2c70ad7c9dfbe21a8c72c1494e124f09d
Sourya Gupta <sourya7909@gmail.com>
2020-11-23 01:20:51 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todo from "./Components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    items: [],
+    currentItem: {
+      text: "",
+      key: "",
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  };
+
+  addItem = (e) => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
+      console.log(newItems);
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  };
+
+  handleDelete = (key) => {
+    const del = this.state.items.filter((item) => item.key !== key);
+    this.setState({ items: del });
+  };
+
+  handleUpdate = (text, key) => {
+    const items = this.state.items;
+    items.map((item) => {
+      if (item.key === key) {
+        item.text = text;
+      }
+    });
+    this.setState({
+      items: items,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TO-DOs</h1>
+        <form onSubmit={this.addItem} className="todo-form">
+          <input
+            type="text"
+            placeholder="Enter text"
+            value={this.state.currentItem.text}
+            onChange={this.handleChange}
+          />
+          <button>Add item</button>
+        </form>
+        <Todo
+          item={this.state.items}
+          deleteTodo={this.handleDelete}
+          updateTodo={this.handleUpdate}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Todo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Todo.css";
+
+const Todo = (props) => {
+  const items = props.item;
+  const itemsList = items.map((item) => {
+    return (
+      <div className="todo_list_card" key={item.key}>
+        <p style={{ display: "inline"}}>
+          <input
+            type="text"
+            value={item.text}
+            onChange={(e) => props.updateTodo(e.target.value, item.key)}
+          />
+        </p>
+        <button style={{position: "absolute", right: "10px", marginTop: "6px"}} onClick={() => props.deleteTodo(item.key)}>Delete</button>
+      </div>
+    );
+  });
+  return <div className="todo_list">{itemsList}</div>;
+};
+
+export default Todo;
